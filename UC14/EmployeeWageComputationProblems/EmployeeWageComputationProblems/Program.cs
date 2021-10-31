aa19c5e44ce4291ab3292bc4a7862f4eb5e7a3a6
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-10-31 17:12:46 +0530

    added:Total emp wage when queired by company

---
+++ UC14/EmployeeWageComputationProblems/EmployeeWageComputationProblems/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EmployeeWageComputationProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
+            Console.WriteLine("Total wage for Dmart company is= " + empWageBuilder.getTotalWage("Dmart"));
+            Console.WriteLine("Total wage for Dmart company is= " + empWageBuilder.getTotalWage("Reliance"));
+        }
+    }
+}
