4d0e7c9180fbf81ec46542f78de71a3d0edbef23
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-10-31 16:36:50 +0530

    Added: to refactor use of interface

---
+++ UC11/EmployeeWageComputationProblems/EmployeeWageComputationProblems/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmployeeWageComputationProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
+        }
+    }
+}
