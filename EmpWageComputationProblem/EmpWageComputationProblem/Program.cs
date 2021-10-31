c711369ddd9c475af720a3a389a19625bb72e4f6
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-10-31 10:33:37 +0530

    Added:Welcome message

---
+++ EmpWageComputationProblem/EmpWageComputationProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWageComputationProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage computation program");
+        }
+    }
+}
